300662cfa535f612fd95184cf7413ac2152b0ffa
The24thDS <davidsima@windowslive.com>
2020-12-16 20:54:11 +0200

    NKB-13 Added Algolia logo and fixed results hiding on inside click

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,13 +1,14 @@
-<div class="w-1/2 relative" x-data="{open: false, searchTerm: @entangle('searchTerm')}">
-  <x-jet-input type="text" wire:model.debounce.500ms="searchTerm" class="w-full" x-on:focus="open=true"
-    x-on:blur="open=false" placeholder="Search" />
-  <ul x-show="open && !!searchTerm" x-transition:enter="transition ease-out duration-150"
+<div class="w-1/2 relative" x-data="{inputFocus: false, searchTerm: @entangle('searchTerm'), resultsFocus: false}">
+  <x-jet-input type="text" wire:model.debounce.500ms="searchTerm" class="w-full" x-on:focus="inputFocus=true"
+    x-on:blur.debounce.100ms="inputFocus=false" placeholder="Search" />
+  <ul x-show="resultsFocus || (inputFocus && !!searchTerm)" x-on:click="resultsFocus=true"
+    x-on:click.away="resultsFocus=false" x-transition:enter="transition ease-out duration-150"
     x-transition:enter-start="opacity-0 transform -translate-y-2"
     x-transition:enter-end="opacity-100 transform translate-y-0" x-transition:leave="transition ease-in duration-150"
     x-transition:leave-start="opacity-100 transform translate-y-0"
     x-transition:leave-end="opacity-0 transform -translate-y-2" class="absolute w-full bg-white shadow-xl z-20 p-4">
     @forelse ($articles as $article)
-      <li class="py-2 border-b my-2 flex justify-between">
+      <li class="py-2 border-b flex justify-between">
         <div class="flex flex-col pl-4">
           <a href="{{ route('article.show', ['article' => $article->id]) }}"
             class="text-lg font-semibold hover:underline">
@@ -32,8 +33,14 @@
         <li>{{ __('general.text.no_items_found_matching', ['items' => 'articles']) }}</li>
       @endif
     @endforelse
-    <li>
+    <li class="my-2 mt-4">
       {{ optional($articles)->links() }}
+    </li>
+    <li class="flex justify-end mt-4">
+      <a href="https://www.algolia.com">
+        <img
+          src="https://res.cloudinary.com/hilnmyskv/image/upload/q_auto/v1607432709/Algolia_com_Website_assets/images/shared/algolia_logo/search-by-algolia-light-background.svg" />
+      </a>
     </li>
   </ul>
 </div>
