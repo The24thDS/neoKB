dedd1e2d9033def60459a4d21084a7590e3b4519
The24thDS <davidsima@windowslive.com>
2020-12-23 15:15:30 +0200

    Added profile page

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\CollectionHelper;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -48,7 +49,7 @@ class UsersController extends Controller
    */
   public function show(User $user)
   {
-    //
+    return view('user.show', compact('user'));
   }
 
   /**
